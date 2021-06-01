9ff42b1bff32f8ab34e1661cf0df273efc3c651d
Jacob Leiner <jacob.leiner@gmail.com>
2021-06-01 15:14:32 -0700

    figured out how to parse soup Result Set into dictionary

--- tasks.py
+++ tasks.py
@@ -1,6 +1,5 @@
 # tasks.py
 import json  # exporting to files
-import re
 from datetime import datetime, timedelta, date
 
 import requests  # pulling data
@@ -49,53 +48,17 @@ def nasdaqtrader_halts_rss():
             yesterday_date_str, today_date_str)
         print(query_string)
         r = requests.get(query_string)
-        soup = BeautifulSoup(r.content, features='html.parser')
+        soup = BeautifulSoup(r.content, 'lxml')
         # select only the "items" I want from the data
-        items = soup.find_all('item')
-        item_dict = {n: list(row) for n in range(len(items)) for row in items}
-        rows = [row[3:13] for row in item_dict.values()]
-        # stripped_rows = list(map(remove_html_tags, rows))
-        for row in rows:
-            print(row[0])
-            print()
-        # print(rows)
-        # print(items)
-        tables = []
-        # items is iterable
-        # print(tables)
+        trade_halts = soup.find_all('item')
+        # create a dictionary from soup Result Set for each halt and add to the current query's list
+        for trade_halt in trade_halts:
+            trade_halt_record = {child.name: child.text for child in trade_halt.findChildren()}
+            trade_halt_list.append(trade_halt_record)
 
-        # for each "item" I want, parse it into a list
-        for halt in tables:
-            symbol = halt.find('ndaq:IssueSymbol').text
-            company_name = halt.find('ndaq:IssueName').text
-            halt_code = halt.find('ndaq:ReasonCode').text
-            halt_date = halt.find('ndaq:HaltDate').text
-            halt_time = halt.find('ndaq:HaltTime').text
-            resumption_date = halt.find('ndaq:ResumptionDate').text
-            resumption_time = halt.find('ndaq:ResumptionTime').text
-            # create an "halt_record" object with the data
-            # from each "item"
-            halt_record = {
-                'symbol': symbol,
-                'company_name': company_name,
-                'halt_code': halt_code,
-                'halt_date': halt_date,
-                'halt_time': halt_time,
-                'resumption_date': resumption_date,
-                'resumption_time': resumption_time,
-                'created_at': str(datetime.now()),
-                'source': 'NasdaqTrader RSS'
-            }
-            print(halt_record)
-            # append my "trade_halt_list" with each "halt_record" object
-            trade_halt_list.append(halt_record)
         print('Finished scraping the trade halts')
-        print(trade_halt_list)
         # after the loop, dump my saved objects into a .json file
         return save_function(trade_halt_list)
     except Exception as e:
         print('The scraping job failed. See exception: ')
         print(e)
-
-
-
